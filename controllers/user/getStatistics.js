a74db902b31e87c5ce02f1a2c89dafa2b3a829a7
yuriyaza <yuriy.aza@gmail.com>
2023-09-17 01:48:09 +0300

    Add extended statistics, calculate BMR, default user icon

--- controllers/user/getStatistics.js
+++ controllers/user/getStatistics.js
@@ -1,13 +1,8 @@
 const { Food, Water } = require('../../models');
-const { asyncHandler } = require('../../utils');
+const { nutrientsByFoodType, nutrientsTotalPerDay, asyncHandler } = require('../../utils');
 
 const getStatistics = asyncHandler(async (req, res) => {
-    let waterUsed = 0;
-    let caloriesUsed = 0;
-    let carbohydratesUsed = 0;
-    let proteinUsed = 0;
-    let fatUsed = 0;
-
+    const user = req.user;
     const owner = req.user._id;
     const requestDate = req.body.date || new Date();
 
@@ -15,44 +10,22 @@ const getStatistics = asyncHandler(async (req, res) => {
     const endDate = new Date(requestDate);
     endDate.setDate(endDate.getDate() + 1); // Збільшуємо на 1 день
 
-    const waterData = await Water.findOne({ owner, date: { $gte: beginDate, $lt: endDate } });
-    if (waterData) {
-        waterUsed = waterData.water;
-    }
-
     const foodData = await Food.findOne({ owner, date: { $gte: beginDate, $lt: endDate } });
-    if (foodData) {
-        const dishes = [...foodData.breakfast, ...foodData.lunch, ...foodData.dinner, ...foodData.snack];
-        dishes.forEach(dish => {
-            caloriesUsed = caloriesUsed + dish.calories;
-            carbohydratesUsed = carbohydratesUsed + dish.carbohydrates;
-            proteinUsed = proteinUsed + dish.protein;
-            fatUsed = fatUsed + dish.fat;
-        });
-    }
+    const waterData = await Water.findOne({ owner, date: { $gte: beginDate, $lt: endDate } });
+
+    const breakfast = nutrientsByFoodType(foodData, 'breakfast');
+    const lunch = nutrientsByFoodType(foodData, 'lunch');
+    const dinner = nutrientsByFoodType(foodData, 'dinner');
+    const snack = nutrientsByFoodType(foodData, 'snack');
+    const total = nutrientsTotalPerDay(user, waterData, foodData);
 
     res.status(200).json({
         date: requestDate,
-        water: {
-            goal: 1500,
-            used: waterUsed.toFixed(0),
-        },
-        calories: {
-            goal: 1700,
-            used: caloriesUsed.toFixed(0),
-        },
-        carbohydrates: {
-            goal: 170,
-            used: carbohydratesUsed.toFixed(2),
-        },
-        protein: {
-            goal: 127.5,
-            used: proteinUsed.toFixed(2),
-        },
-        fat: {
-            goal: 56,
-            used: proteinUsed.toFixed(2),
-        },
+        breakfast,
+        lunch,
+        dinner,
+        snack,
+        total,
     });
 });
 
--- utils/index.js
+++ utils/index.js
@@ -2,10 +2,16 @@ const { asyncHandler } = require('./asyncHandler');
 const { throwHttpError } = require('./throwHttpError');
 const { sendRestoreEmail } = require('./sendRestoreEmail');
 const { handleMongooseError } = require('./handleMongooseError');
+const { calculateBMR } = require('./calculateBMR');
+const { nutrientsByFoodType } = require('./nutrientsByFoodType');
+const { nutrientsTotalPerDay } = require('./nutrientsTotalPerDay');
 
 module.exports = {
     asyncHandler,
     throwHttpError,
     sendRestoreEmail,
     handleMongooseError,
+    calculateBMR,
+    nutrientsByFoodType,
+    nutrientsTotalPerDay,
 };
