0d00e0c89c6d03d18b3b2bc9c62c03bde35b65c1
Volodymyr Bondar <2005volka@gmail.com>
2023-09-18 20:58:02 +0300

    cloudinary problem

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,11 +1,13 @@
 const path = require('node:path');
 const fs = require('node:fs/promises');
 const sharp = require('sharp');
+// const cloudinary = require('cloudinary').v2;
 
 const { Users } = require('../../models');
 const { asyncHandler } = require('../../utils');
 
 const updateAvatar = asyncHandler(async (req, res) => {
+    // try {
     const { _id } = req.user;
     const { filename } = req.file;
 
@@ -16,13 +18,18 @@ const updateAvatar = asyncHandler(async (req, res) => {
     await sharp(tempFile).resize(250, 250, 'contain').toFile(destFile);
     await fs.unlink(tempFile);
 
-    const updatedUser = await Users.findByIdAndUpdate(_id, { avatar }, { returnDocument: 'after' });
+    // const result = await cloudinary.uploader.upload(destFile);
 
+    const updatedUser = await Users.findByIdAndUpdate(_id, { avatar }, { returnDocument: 'after' }); // avatar: result.secure_url замість просто avatar
     res.status(200).json({
         user: {
-            avatar: updatedUser.avatar
+            avatar: updatedUser.avatar,
         },
     });
+    // } catch (error) {
+    //     console.error(error);
+    //     res.status(500).json({ error: 'Error to upload the avatar' });
+    // }
 });
 
 module.exports = { updateAvatar };
