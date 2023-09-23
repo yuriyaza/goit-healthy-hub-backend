61d871ac9df7f88d56cede1c6801237b9a1622bc
yuriyaza <yuriy.aza@gmail.com>
2023-09-23 19:11:12 +0300

    Modify POST food-intake

--- controllers/user/foodIntake.js
+++ controllers/user/foodIntake.js
@@ -1,50 +1,81 @@
-const { Food } = require('../../models');
-const { asyncHandler } = require('../../utils');
-const { throwHttpError } = require('../../utils/throwHttpError');
+const { Food, Water } = require('../../models');
+const { nutrientsTotalPerTime, nutrientsByFoodType, nutrientsTotalPerDay, asyncHandler } = require('../../utils');
 
 const foodIntake = asyncHandler(async (req, res) => {
-    const { _id: owner } = req.user;
-    const { date, breakfast, lunch, dinner, snack } = req.body;
+    const user = req.user;
+    const owner = req.user._id;
+    const { breakfast, lunch, dinner, snack } = req.body;
+    const requestDate = req.body.date || new Date();
 
-    try {
-        const currentDate = new Date(date);
-        const startOfDay = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
-        const endOfDay = new Date(startOfDay);
-        endOfDay.setHours(23, 59, 59, 999);
+    const currentDate = new Date(requestDate);
+    const startOfDay = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
+    const endOfDay = new Date(startOfDay);
+    endOfDay.setHours(23, 59, 59, 999);
 
-        let existingFoodEntry = await Food.findOne({ owner, date: { $gte: startOfDay, $lte: endOfDay } });
+    const existingFoodEntry = await Food.findOne({ owner, date: { $gte: startOfDay, $lte: endOfDay } });
+    let savedFoodEntry = null;
 
-        if (existingFoodEntry) {
-            if (breakfast) {
-                existingFoodEntry.breakfast = existingFoodEntry.breakfast.concat(breakfast);
-            }
-            if (lunch) {
-                existingFoodEntry.lunch = existingFoodEntry.lunch.concat(lunch);
-            }
-            if (dinner) {
-                existingFoodEntry.dinner = existingFoodEntry.dinner.concat(dinner);
-            }
-            if (snack) {
-                existingFoodEntry.snack = existingFoodEntry.snack.concat(snack);
-            }
-
-            await existingFoodEntry.save();
-            res.status(200).json(existingFoodEntry);
-        } else {
-            const foodEntry = new Food({
-                owner,
-                date: currentDate,
-                breakfast,
-                lunch,
-                dinner,
-                snack,
-            });
-            const savedFoodEntry = await foodEntry.save();
-            res.status(201).json(savedFoodEntry);
+    if (existingFoodEntry) {
+        if (breakfast) {
+            existingFoodEntry.breakfast = existingFoodEntry.breakfast.concat(breakfast);
         }
-    } catch (error) {
-        throwHttpError(500, 'Error saving food entry');
+        if (lunch) {
+            existingFoodEntry.lunch = existingFoodEntry.lunch.concat(lunch);
+        }
+        if (dinner) {
+            existingFoodEntry.dinner = existingFoodEntry.dinner.concat(dinner);
+        }
+        if (snack) {
+            existingFoodEntry.snack = existingFoodEntry.snack.concat(snack);
+        }
+        savedFoodEntry = await existingFoodEntry.save();
+    } else {
+        const foodEntry = new Food({
+            owner,
+            date: currentDate,
+            breakfast,
+            lunch,
+            dinner,
+            snack,
+        });
+        savedFoodEntry = await foodEntry.save();
     }
+
+    // const existingWaterEntry = await Water.findOne({ owner, date: { $gte: startOfDay, $lte: endOfDay } });
+
+    // const nutrientsBreakfast = nutrientsByFoodType(savedFoodEntry, 'breakfast');
+    // const nutrientsLunch = nutrientsByFoodType(savedFoodEntry, 'lunch');
+    // const nutrientsDinner = nutrientsByFoodType(savedFoodEntry, 'dinner');
+    // const nutrientsSnack = nutrientsByFoodType(savedFoodEntry, 'snack');
+    // const nutrientsTotal = nutrientsTotalPerDay(user, existingWaterEntry, savedFoodEntry);
+
+   let breakfastDishes = [];
+   let lunchDishes = [];
+   let dinnerDishes = [];
+   let snackDishes = [];
+
+    if (savedFoodEntry) {
+        breakfastDishes = savedFoodEntry.breakfast;
+        lunchDishes = savedFoodEntry.lunch;
+        dinnerDishes = savedFoodEntry.dinner;
+        snackDishes = savedFoodEntry.snack;в
+    }
+
+    const breakfastTotal = nutrientsTotalPerTime(breakfastDishes);
+    const lunchTotal = nutrientsTotalPerTime(lunchDishes);
+    const dinnerTotal = nutrientsTotalPerTime(dinnerDishes);
+    const snackTotal = nutrientsTotalPerTime(snackDishes);
+
+    res.status(200).json({
+        breakfast: breakfastTotal,
+        lunch: lunchTotal,
+        dinner: dinnerTotal,
+        snack: snackTotal,
+        breakfastDishes,
+        lunchDishes,
+        dinnerDishes,
+        snackDishes,
+    });
 });
 
 module.exports = { foodIntake };
