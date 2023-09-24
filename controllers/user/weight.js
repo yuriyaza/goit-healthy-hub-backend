cb7089f2819c97694323359f0042e3a4591e3497
yuriyaza <yuriy.aza@gmail.com>
2023-09-24 23:20:12 +0300

    Fix weight update

--- controllers/user/weight.js
+++ controllers/user/weight.js
@@ -1,10 +1,15 @@
-const { Weight } = require('../../models');
+const { Users, Weight } = require('../../models');
 const { asyncHandler } = require('../../utils');
 
 const weight = asyncHandler(async (req, res) => {
-    const { _id: owner } = req.user;
-    const currentDate = req.body.date || Date.now();
+    const { _id } = req.user;
+    const owner = String(_id);
+
+    const currentDate = req.body.date || new Date();
     const newWeight = req.body.weight;
+
+    // Оновлюємо вагу у властивостях користувача (таблиця users)
+    await Users.findByIdAndUpdate(_id, { weight: newWeight }, { new: true });
 
     // Дата - об'єкт, тому створюємо два окремі об'єкти, щоб .setHours не затер currentDate
     // Задаємо для вибірки початок та кінець доби
@@ -13,6 +18,7 @@ const weight = asyncHandler(async (req, res) => {
     beginDate.setHours(0, 0, 0, 0);
     endDate.setHours(23, 59, 59, 999);
 
+    // Робимо запис про вагу в щоденник ваги (таблиця weights)
     // Перевіряємо, чи вносив вже поточний користувач дані за поточну добу
     const currentUserAndDay = await Weight.findOne({ owner, date: { $gte: beginDate, $lte: endDate } });
 
@@ -24,7 +30,10 @@ const weight = asyncHandler(async (req, res) => {
         updatedUserAndDay = await Weight.findByIdAndUpdate(currentUserAndDay._id, { weight: newWeight }, { new: true });
     }
 
-    res.status(200).json(updatedUserAndDay);
+    res.status(200).json({
+        date: updatedUserAndDay.date,
+        weight: updatedUserAndDay.weight,
+    });
 });
 
 module.exports = { weight };
